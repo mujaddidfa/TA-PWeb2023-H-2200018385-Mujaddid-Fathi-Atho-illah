e87b79c9d2120899ae3d439905a0b7ca530e4600
Mujaddid Fathi Athoillah <mujaddid201@gmail.com>
2023-06-13 22:30:20 +0700

    modifikasi tugas09

--- PWeb2023-Tugas-09-H-2200018385-Mujaddid Fathi Atho'illah.php
+++ PWeb2023-Tugas-09-H-2200018385-Mujaddid Fathi Atho'illah.php
@@ -6,3 +6,17 @@
     echo "Gaji sebelum pajak = Rp. $gaji <br>";
     echo "Gaji yang dibawa pulang = Rp. $thp <br>";
 ?>
+
+<br><br>
+
+<?php
+    $a = 5;
+    $b = 4;
+
+    echo "$a == $b : ". ($a == $b);
+    echo "<br>$a != $b : ". ($a != $b);
+    echo "<br>$a > $b : ". ($a > $b);
+    echo "<br>$a < $b : ". ($a < $b);
+    echo "<br>($a == $b) && ($a > $b) : ". (($a == $b) && ($a > $b));
+    echo "<br>($a == $b) || ($a > $b) : ". (($a == $b) || ($a > $b));
+?>
